b7d273b3c77cffe3245b9f28e86edab111455ff3
Chester Lim <chesterlim2004@gmail.com>
2026-08-20 16:56:32 +0800

    Greet and exit print

--- src/main/java/Crystal.java
+++ src/main/java/Crystal.java
@@ -1,10 +1,21 @@
+/**
+ * Greets the user as Crystal and exits.
+ */
 public class Crystal {
     public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
         String banner = "  ____ ______   ______ _____  _    _\n"
                 + " / ___|  _ \\ \\ / / ___|_   _|/ \\  | |\n"
                 + "| |   | |_) \\ V /\\___ \\ | | / _ \\ | |\n"
                 + "| |___|  _ < | |  ___) || |/ ___ \\| |___\n"
                 + " \\____|_| \\_\\|_| |____/ |_/_/   \\_\\_____|\n";
-        System.out.println(banner);
+
+        System.out.println(horizontalLine);
+        System.out.print(banner);
+        System.out.println("\nHello!!! I'm Crystal.");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye!!! Hope to see you again soon!");
+        System.out.println(horizontalLine);
     }
 }
