cdcd74e5fa7eab572184d4e967736eccaf714149
Chester Lim <chesterlim2004@gmail.com>
2026-08-20 17:21:34 +0800

    Basic list functionality

--- src/main/java/Crystal.java
+++ src/main/java/Crystal.java
@@ -1,9 +1,14 @@
 import java.util.Scanner;
 
 /**
- * Greets the user, echoes commands, and exits when the user enters {@code bye}.
+ * Stores tasks entered by the user, lists them on request, and exits on {@code bye}.
  */
 public class Crystal {
+    /**
+     * Runs the chatbot's command loop.
+     *
+     * @param args command-line arguments; not used
+     */
     public static void main(String[] args) {
         String horizontalLine = "____________________________________________________________";
         String banner = "  ____ ______   ______ _____  _    _\n"
@@ -11,14 +16,19 @@ public class Crystal {
                 + "| |   | |_) \\ V /\\___ \\ | | / _ \\ | |\n"
                 + "| |___|  _ < | |  ___) || |/ ___ \\| |___\n"
                 + " \\____|_| \\_\\|_| |____/ |_/_/   \\_\\_____|\n";
+        String instructions = "[Commands:\n"
+                + "- To exit, enter 'bye'\n"
+                + "- To view your list, enter 'list']";
 
         System.out.println(horizontalLine);
         System.out.print(banner);
         System.out.println("\nHello!!! I'm Crystal.");
-        System.out.println("What can I do for you? (To exit, enter 'bye')");
+        System.out.println(instructions);
         System.out.println(horizontalLine);
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         while (true) {
             System.out.print("You: ");
             String command = scanner.nextLine();
@@ -27,7 +37,15 @@ public class Crystal {
             }
 
             System.out.println(horizontalLine);
-            System.out.println("Crystal: " + command);
+            if (command.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("Crystal: " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println("Crystal: added: " + command);
+            }
             System.out.println(horizontalLine);
         }
 
